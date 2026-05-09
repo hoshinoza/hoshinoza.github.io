b88d675232dc4a730680b7e5cec384bf34bc7826
hoshinoza <hoshinoza.constellation@gmail.com>
2026-05-09 17:20:47 +0900

    modify links

--- src/components/SocialLinks.tsx
+++ src/components/SocialLinks.tsx
@@ -5,17 +5,27 @@ const SocialLinks: React.FC = () => {
   return (
     <footer className="social-links">
       <div className="glass-card">
-        <h3>Connect with 星ノ座</h3>
-        <div className="links-container">
-          <a href="https://www.youtube.com/channel/UCJZnKoctpCvwRFDoYNoLnjA" target="_blank" rel="noopener noreferrer" className="link-item youtube">
-            YouTube
-          </a>
-          <a href="https://x.com/hoshinoza" target="_blank" rel="noopener noreferrer" className="link-item x">
-            X (Twitter)
-          </a>
-          <a href="https://www.nicovideo.jp/user/97505658/video" target="_blank" rel="noopener noreferrer" className="link-item niconico">
-            NicoNico
-          </a>
+        <div className="social-sections">
+          <div className="social-section">
+            <h4>Music & Video</h4>
+            <div className="links-container">
+              <a href="https://www.youtube.com/channel/UCJZnKoctpCvwRFDoYNoLnjA" target="_blank" rel="noopener noreferrer" className="link-item youtube">
+                YouTube
+              </a>
+              <a href="https://www.nicovideo.jp/user/97505658/video" target="_blank" rel="noopener noreferrer" className="link-item niconico">
+                NicoNico
+              </a>
+            </div>
+          </div>
+          
+          <div className="social-section">
+            <h4>Information</h4>
+            <div className="links-container">
+              <a href="https://x.com/hoshinoza" target="_blank" rel="noopener noreferrer" className="link-item x">
+                X (Twitter)
+              </a>
+            </div>
+          </div>
         </div>
         <p className="copyright">&copy; {new Date().getFullYear()} 星ノ座 (Hoshinoza)</p>
       </div>
